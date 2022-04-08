49d36f1190559e5e0cea6b52fff4296d98ae4e67
daon1213 <diaeiyo1213@naver.com>
2022-04-08 09:06:06 +0900

    MainActivity commit!

---
+++ app/src/main/java/com/daon/push_part2_01/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.daon.push_part2_01
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.txt_result)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.txt_token)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
+    }
+}
